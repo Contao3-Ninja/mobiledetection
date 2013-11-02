6b98331123867f5f52916e6895cd6d1c77fe298b
Glen Langer <contao.modul@glen-langer.de>
2013-11-02 18:39:24 +0100

    Version 3.3.0

--- classes/Mobile_Detection.php
+++ classes/Mobile_Detection.php
@@ -32,7 +32,7 @@ class Mobile_Detection extends \System
     /**
      * Current version of the classes.
      */
-    const MOBILE_DETECTION_VERSION  = '3.1.0'; // Version of this class
+    const MOBILE_DETECTION_VERSION  = '3.3.0'; // Version of this class
 
 
 	/**
